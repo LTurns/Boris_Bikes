102a2fb18c84bdcb89f0b6df9a95bbaad8817a20
LTurns <lizzieturney@gmail.com>
2020-04-16 18:17:13 +0100

    interacting bike class with docking class

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,4 +3,9 @@ require 'docking-station'
 RSpec.describe DockingStation do
   #instance = DockingStation.new
   it { is_expected.respond_to?:release_bike }
+
+  it 'releases working bikes' do
+    bike = (subject).release_bike
+    expect(bike).respond_to?:working
+  end
 end
